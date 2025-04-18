4045252a65d2aed88713aabb9f2bb720a0973e25
ossikuro <ossikuro@gmail.com>
2025-04-18 14:24:22 +0300

    mobile menu without animation

--- public/header.js
+++ public/header.js
@@ -1,3 +1,4 @@
+// Search
 const toggle = document.getElementById("search-toggle");
 const searchWrapper = document.querySelector(".search_wrapper");
 
@@ -7,4 +8,41 @@ toggle.addEventListener("change", () => {
   } else {
     searchWrapper.classList.remove("active");
   }
+});
+
+
+// Menu
+const mobileMenuButton = document.querySelector('.burger_menu');
+const mobileCloseButton = document.getElementById('closeMenu');
+const mobileNavbar = document.querySelector('.mobile_menu');
+
+function openMenu() {
+    if (window.innerWidth <= 640) {
+        mobileNavbar.style.display = 'block';
+        mobileNavbar.style.visibility = 'visible'; // Ensure visibility
+        document.documentElement.style.overflow = 'hidden'; // Disable scrolling on the page
+    }
+}
+
+// close the menu
+function closeMenu() {
+    mobileNavbar.style.display = 'none';
+    mobileNavbar.style.visibility = 'hidden'; // Hide visibility
+    document.documentElement.style.overflow = ''; // Enable scrolling again
+}
+
+// event launchers
+mobileMenuButton.addEventListener('click', openMenu);
+mobileCloseButton.addEventListener('click', closeMenu);
+
+// hide the menu on loading the page
+window.addEventListener('load', () => {
+    mobileNavbar.style.display = 'none';
+    mobileNavbar.style.visibility = 'hidden';
+});
+
+// hide the menu when open new page
+window.addEventListener('beforeunload', () => {
+    mobileNavbar.style.display = 'none';
+    mobileNavbar.style.visibility = 'hidden';
 });
