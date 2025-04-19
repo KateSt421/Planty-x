917c5f3c601fb209ffe1446d874376f24276a0ef
irinavladimirovna1307 <magstroiardi@gmail.com>
2025-04-19 14:50:46 +0300

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,10 @@
-import globals from 'globals'
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
 
-;/ @type {import('eslint').Linter.Config[]} */
-export default [
-  {
-    languageOptions: {
-      parserOptions: {
-        sourceType: 'module', // Поддержка ES-модулей
-      },
-    },
-    ignores: ['node_modules/', 'dist/'], // Игнорировать ненужные папки
-  },
-  {
-    files: ['/*.js'], // Применяется к JS-файлам
-    languageOptions: {
-      sourceType: 'module', // Поддержка ES-модулей
-      globals: globals.browser, // Глобальные переменные браузера
-    },
-    rules: {
-      'no-unused-vars': 'warn', // Предупреждение для неиспользуемых переменных
-      'no-console': 'off', // Разрешить использование console.log
-    },
-  },
-]
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+]);
